e63a34dae5c0959a0b81016b1b4a416ba7f3be98
fh <fhfanhui@gmail.com>
2024-09-20 20:21:18 +0800

    ..

--- tools/init.sh
+++ tools/init.sh
@@ -11,6 +11,8 @@ sleep 1
 
 rm -rf *data
 
+mkdir gethdata beacondata validatordata || exit 1
+
 ./prysmctl testnet generate-genesis --fork capella --num-validators 64 --genesis-time-delay 10 --config-name interop --chain-config-file config.yml --geth-genesis-json-in genesis.json  --geth-genesis-json-out genesis.json --output-ssz genesis.ssz --output-json genesis.ssz.json || exit 1
 
 ./geth --datadir=gethdata account import --password secret.txt.password secret.txt || exit 1
@@ -22,6 +24,8 @@ nohup ./geth --http --http.addr=0.0.0.0 --http.api eth,web3,net,txpool,shh,debug
 nohup ./beacon-chain --datadir beacondata --min-sync-peers 0 --genesis-state genesis.ssz --bootstrap-node= --interop-eth1data-votes --chain-config-file config.yml --contract-deployment-block 0 --chain-id 9527000 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0x123463a4B065722E99115D6c222f267d9cABb524 --minimum-peers-per-subnet 0 --enable-debug-rpc-endpoints --execution-endpoint gethdata/geth.ipc >beacondata/log.txt 2>&1 &
 
 nohup ./validator --datadir validatordata --accept-terms-of-use --interop-num-validators 64 --chain-config-file config.yml >validatordata/log.txt 2>&1 &
+
+sleep 3
 
 tail -n 3 gethdata/log.txt
 tail -n 3 beacondata/log.txt
